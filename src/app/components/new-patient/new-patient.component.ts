ccf20f3071e1139c0b6e5faa2c5e1fd933cfad8e
fcoverdugo <fcoverdugoa@underpost.net>
2022-04-25 16:23:55 -0400

    update

--- src/app/components/new-patient/new-patient.component.ts
+++ src/app/components/new-patient/new-patient.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Paciente } from 'src/app/models/paciente.model';
+import { PacientesService } from 'src/app/services/pacientes/pacientes.service';
 
 @Component({
   selector: 'app-new-patient',
@@ -12,11 +13,22 @@ export class NewPatientComponent implements OnInit {
   
   public pacienteForm: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private pacientesService: PacientesService) {
 
     this.pacienteForm = this.fb.group({
-      nombre: ['', Validators.required ]
+      nombre: ['', Validators.required ],
+      rut: ['', Validators.required ],
+      edad: ['', Validators.required ],
+      fechaIngreso: ['', Validators.required],
+      enfermedad: ['', Validators.required]
     });
+
+    /*
+        sexo: String;
+        revisado: Boolean;
+        fotoPersonal: String;
+        
+    */
 
    }
 
@@ -25,9 +37,37 @@ export class NewPatientComponent implements OnInit {
     
   }
 
-  savePaciente(){
+  // eventos
+
+  async savePaciente(){
     console.log("savePaciente", this.pacienteForm.value);
 
+    if(this.pacienteForm.invalid){
+      return alert('campos invalidos');
+    }
+
+    const postObj: Paciente = {
+      ...this.pacienteForm.value
+    };
+    await this.createPaciente(postObj);
+
+  }
+  
+  // servicios 
+  
+  async createPaciente(postObj: Paciente){
+    return new Promise( (resolve, reject) => 
+      this.pacientesService.postPaciente(postObj).subscribe( 
+        response => {
+          console.log("success postPaciente() ", response);
+          resolve(response);
+        },
+        error => {
+          console.log("error postPaciente() ", error);
+          reject(error);
+        }
+      )
+    )
   }
 
 }
--- src/app/services/pacientes/pacientes.service.ts
+++ src/app/services/pacientes/pacientes.service.ts
@@ -2,6 +2,7 @@
 import { API_URL } from "../global.service";
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Paciente } from "src/app/models/paciente.model";
 
 @Injectable()
 export class PacientesService {
@@ -15,6 +16,11 @@ export class PacientesService {
     getPacientes() {
         console.log("getPacientes()", this.SERVICE_URL);
         return this.http.get(this.SERVICE_URL);
+    }
+
+    postPaciente(postObj: Paciente) {
+        console.log("postPaciente()", postObj);
+        return this.http.post(this.SERVICE_URL, postObj);
     }
 
 }
