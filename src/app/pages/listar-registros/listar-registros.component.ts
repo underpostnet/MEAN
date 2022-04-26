e9b144dac91beb759df53f14e563bdd65c86e132
fcoverdugo <fcoverdugoa@underpost.net>
2022-04-26 11:29:33 -0400

    update

--- src/app/pages/listar-registros/listar-registros.component.ts
+++ src/app/pages/listar-registros/listar-registros.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { PacientesService } from 'src/app/services/pacientes/pacientes.service';
 
 @Component({
   selector: 'app-listar-registros',
@@ -8,11 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarRegistrosComponent implements OnInit {
 
-  constructor() { }
+  public deletePacienteForm: FormGroup;
+  displayTable: boolean = true;
+
+  constructor(private fb: FormBuilder, private pacientesService: PacientesService) { 
+    this.deletePacienteForm = this.fb.group({
+      id: ['', Validators.required ]
+    });
+  }
 
   ngOnInit(): void {
   }
 
- 
+  async deletePaciente(){
+    this.pacientesService.deletePaciente(this.deletePacienteForm.value.id).subscribe( 
+      response => {
+        console.log("success deletePaciente() ", response);
+        this.displayTable = false;
+        this.deletePacienteForm.reset();
+        setTimeout( () => this.displayTable = true, 0);        
+      },
+      error => {
+        console.log("error deletePaciente() ", error);
+      }
+    )
+  } 
 
 }
