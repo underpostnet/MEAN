d04ba14e60862ec431d80d72e75bdcdd5a7abcf6
fcoverdugo <fcoverdugoa@underpost.net>
2022-04-26 14:26:11 -0400

    update

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { Paciente } from 'src/app/models/paciente.model';
 import { PacientesService } from 'src/app/services/pacientes/pacientes.service';
 
@@ -17,7 +18,7 @@ export class SearchComponent implements OnInit {
   public searchPacienteForm: FormGroup;
   displayTable: boolean = true;
 
-  constructor(private fb: FormBuilder, private pacientesService: PacientesService) { 
+  constructor(private fb: FormBuilder, private pacientesService: PacientesService, private router: Router) { 
     this.searchPacienteForm = this.fb.group({
       id: ['', Validators.required ]
     });
@@ -26,9 +27,9 @@ export class SearchComponent implements OnInit {
   ngOnInit(): void {
   }
 
-
   async searchPaciente(){
     console.log(this.searchPacienteForm.value);
+    this.router.navigate(['/registro/detalle/'+this.searchPacienteForm.value.id])
   }
 
 }
